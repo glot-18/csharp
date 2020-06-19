a55d918724b5a0d412b229a7f06296a98caa0247
G Singh <61596435+glot-18@users.noreply.github.com>
2020-06-18 20:15:43 -0600

    Add files via upload

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int num = getlength("Enter in a length", 0, 1000);
+            char c = getchar("Enter a char:");
+            Draw(num,c);
+        }
+
+        static int getlength(string prompt, int low, int high)
+        {
+            bool invalidInput = true;
+            int num = 0;
+
+            while (invalidInput)
+            {
+                Console.WriteLine($"{prompt}: ");
+                num = int.Parse(Console.ReadLine());
+                if (num>1)
+                    invalidInput = false;
+                else
+                    Console.WriteLine("Error: Invalid Input.");
+
+            }
+            return num;
+
+        }
+
+        static char getchar(string prompt)
+        {
+            bool input = true;
+            char c = 'c';
+
+            while (input)
+            {
+                Console.WriteLine("Enter in a Character: ");
+                c = char.Parse(Console.ReadLine());
+                input = false;
+            }
+
+            return c;
+        }
+        static void Draw(int num, char c)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                DrawRow(num, c);
+                Console.Write($"\n");
+            }
+        }
+
+        static void DrawRow(int num, char c)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write($"{c}");
+            }
+        }
+
+
+    }
+}
